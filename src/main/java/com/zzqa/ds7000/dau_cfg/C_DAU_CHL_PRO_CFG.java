528a33f6fb38b100cac533e4263e676700fc2e7a
mymidad <1277420858@qq.com>
2021-01-14 17:56:28 +0800

    .

---
+++ src/main/java/com/zzqa/ds7000/dau_cfg/C_DAU_CHL_PRO_CFG.java
@@ -0,0 +1,12 @@
+package com.zzqa.ds7000.dau_cfg;
+
+/**
+ * ClassName: C_DAU_CHL_PRO_CFG
+ * Description: 工艺量通道信息表
+ *
+ * @author 张文豪
+ * @date 2021/1/14 17:54
+ */
+public class C_DAU_CHL_PRO_CFG {
+
+}
---
+++ src/main/java/com/zzqa/ds7000/dau_cfg/DAU_CHL_PRO_CFG.java
@@ -0,0 +1,16 @@
+package com.zzqa.ds7000.dau_cfg;
+
+/**
+ * ClassName: DAU_CHL_PRO_CFG
+ * Description: 过程量通道信息数据结构描述
+ *
+ * @author 张文豪
+ * @date 2021/1/14 17:51
+ */
+public class DAU_CHL_PRO_CFG {
+    private int dwStructLen;        //字节长度
+
+    private C_DAU_CHL_CFG dau_chl;
+    private C_DAU_CHL_PRO_CFG dau_pro_chl;
+
+}
